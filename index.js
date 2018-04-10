741b0ed60096d171ed69bd32cd83a090b382f5ec
arjunrawal4 <20448923+arjunrawal4@users.noreply.github.com>
2018-04-09 20:23:33 -0500

    adding server

---
+++ index.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/static'));
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/base.css');
+
+});
+
+io.on('connection', function (socket) {
+  console.log('a user connected');
+  socket.on('disconnect', function () {
+    console.log('user disconnected');
+  });
+});
+
+io.on('connection', function (socket) {
+  socket.on('chat message', function (msg) {
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, function () {
+  console.log('listening on *:3000');
+});
